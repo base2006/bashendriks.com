c8527cc2f9aab8c1572bab66c792e714b45586d3
Bas Hendriks <bashendriks14@msn.com>
2016-09-15 17:26:35 +0200

    fontawesome & form validation

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 
+function e($string) {
+    return htmlentities($string, ENT_QUOTES, 'UTF-8', false);
+}
 
+$errors = [];
+
+if ($_POST && !empty($_POST)) {
+    $fields = array(
+        'name' => $_POST['name'],
+        'email' => $_POST['email'],
+        'message' => $_POST['message']
+    );
+
+    foreach ($fields as $field => $data) {
+        if (empty($data)) {
+            $errors[$field] = 'The ' . $field . ' field is required';
+        }
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -17,9 +35,13 @@
     <!-- Custom Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
 
     <!-- Custom styles -->
     <link rel="stylesheet" href="css/min/main.css" media="screen" title="no title">
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
 
     <!-- Google reCaptcha -->
     <script src='https://www.google.com/recaptcha/api.js'></script>
@@ -27,8 +49,10 @@
   </head>
   <body>
 
-    <section class="container full-height home">
-        <h2 class="v-center hide quote">"The main goal is not to complicate the already difficult life of the consumer." <span class="italic">- Raymond Loewy</span></h2>
+    <section class="container-fluid full-height relative home">
+        <div class="container">
+            <h2 class="v-center hide quote">"The main goal is not to complicate the already difficult life of the consumer." <span class="italic">- Raymond Loewy</span></h2>
+        </div>
     </section>
 
     <section class="container-fluid relative full-height about">
@@ -36,7 +60,7 @@
             <div class="col-md-8">
                 <p>Hi, my name is Bas and I'm a front end developer from the south of the Netherlands.</p>
 
-                <p>Development and front end is what I do and love doing. My main languages of choice are PHP, HTML, SCSS and jQuery. I also know my way around Photoshop, Illustrator and Indesign. Development I prefer doing in <a href="https://atom.io/" target="_blank">Atom</a></p>
+                <p>Development and front end is what I do and love doing. My main languages of choice are PHP, HTML, SCSS and jQuery. I also know my way around Photoshop, Illustrator and Indesign. Development I prefer doing in <a href="https://atom.io/" target="_blank">Atom</a>.</p>
 
                 <p>In my spare time I love to go into the outdoors and hit some trails on my mountainbike. But on a rainy day I prefer the gym.</p>
 
@@ -87,37 +111,59 @@
 
     </section>
 
-    <section class="container relative contact">
-        <h3>Say hi</h3>
-        <p>
-            Feel free to leave me a message, just to say hi or to get in touch.
-        </p>
-        <form class="contact-form" method="post">
-            <div class="form-group">
-                <input type="text" class="form-control" name="name" placeholder="What's your name?" autocomplete="off">
-            </div>
+    <section class="container-fluid relative contact">
+        <div class="container">
+            <h3>Say hi</h3>
+            <p>
+                Feel free to leave me a message, just to say hi or to get in touch.
+            </p>
+            <?php
+            if (!empty($errors)) {
+                foreach ($errors as $errorname => $error){
+                ?>
+                    <script type="text/javascript">
+                        jQuery('#<?= $errorname ?>').addClass('form-control-danger');
+                        jQuery('#<?= $errorname ?>').parent().addClass('has-danger');
+                        jQuery('#<?= $errorname ?>').parent().find('div.form-control-feedback').css('display', 'block');
+                    </script>
+                <?php
+                }
+            }
+            ?>
+            <form class="contact-form" method="post">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="What's your name?" autocomplete="off" value="<?= ($_POST && !empty($_POST['name'])) ? e($_POST['name']) : '' ; ?>">
+                    <?php if (!empty($errors)) :?>
+                        <div class="form-control-feedback hide"><?= $errors['name'] ?></div>
+                    <?php endif; ?>
+                </div>
 
-            <div class="form-group">
-                <input type="email" class="form-control" name="email" placeholder="What's your email?" autocomplete="off">
-            </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="What's your email?" autocomplete="off" value="<?= ($_POST && !empty($_POST['email'])) ? e($_POST['email']) : '' ; ?>">
+                </div>
 
-            <div class="form-group">
-                <textarea name="message" class="form-control" placeholder="Leave your message here"></textarea>
-            </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" name="message" id="message" placeholder="Leave your message here"><?= ($_POST && !empty($_POST['message'])) ? e($_POST['message']) : '' ; ?></textarea>
+                </div>
 
-            <div class="g-recaptcha" data-sitekey="6LeMQhgTAAAAAG1Dl2Is8XzbMjIkMv3SETGfPqRw"></div>
+                <div class="g-recaptcha" data-sitekey="6LeMQhgTAAAAAG1Dl2Is8XzbMjIkMv3SETGfPqRw"></div>
 
-            <input type="submit" class="btn btn-success" value="Send message">
-        </form>
+                <input type="submit" class="btn btn-success" value="Send message">
+
+            </form>
+        </div>
     </section>
 
     <footer>
-        <div class="col-sm-6 copyright">&copy; <?= date('Y') ?> Bas Hendriks </div>
-        <div class="col-sm-6"></div>
+        <div class="container">
+            <div class="col-sm-6 copyright">&copy; <?= date('Y') ?> Bas Hendriks </div>
+            <div class="col-sm-6"></div>
+        </div>
     </footer>
 
-    <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
     <!-- Custom Javascript -->
