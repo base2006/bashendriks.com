be1215ad1640cc09e782d67f0708c7030aa2b5fd
Bas Hendriks <bashendriks14@msn.com>
2016-09-20 17:22:33 +0200

    JS form validation

--- index.php
+++ index.php
@@ -1,73 +1,8 @@
 <?php
-session_start();
-
-function e($string) {
-    return htmlentities($string, ENT_QUOTES, 'UTF-8', false);
-}
-
-$errors = [];
-
-if (isset($_POST)) {
-    // Check if name is set
-    if (empty($_POST['name'])) {
-        $errors['name'] = 'Please fill in your name';
-    }
-
-    // Check if email is set
-    if (empty($_POST['email'])) {
-        $errors['email'] = 'Please fill in your email';
-    } else {
-        // Check if email is valid
-        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors['email'] = 'Please enter a valid email address';
-        }
-    }
-
-    // Check if a message is set
-    if (empty($_POST['message'])) {
-        $errors['message'] = 'Please enter a message to send';
-    }
-
-    if (!empty($errors)) {
-        if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-            echo json_encode($errors);
-            exit;
-        }
-
-        $errors_php = $errors;
-    } else {
-
-    }
-}
-
+    require_once 'includes/functions.php';
+    require_once 'includes/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags always come first -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
-
-    <!-- Custom Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
-    <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
-
-    <!-- Custom styles -->
-    <link rel="stylesheet" href="css/min/main.css" media="screen" title="no title">
-
-    <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
-
-    <!-- Google reCaptcha -->
-    <script src='https://www.google.com/recaptcha/api.js'></script>
-
-  </head>
-  <body>
     <!-- Check if JS is enabled -->
     <noscript>
         <meta HTTP-EQUIV="Refresh" CONTENT="0;URL=noJS.html">
@@ -94,7 +29,9 @@ if (isset($_POST)) {
         </div>
     </section>
 
-    <section class="container full-height hide projects">
+    <section class="container full-height projects">
+        <h2>My Projects</h2>
+        <p>These are some of my project I have worked on. If you have a question about them, leave me a message down below.</p>
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -103,21 +40,21 @@ if (isset($_POST)) {
             </ol>
             <div class="carousel-inner" role="listbox">
                 <div class="carousel-item active">
-                    <img src="..." alt="First slide">
+                    <img src="http://lorempixel.com/640/400" alt="First slide">
+                    <div class="carousel-caption">
+                        <h3>test</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="http://lorempixel.com/640/400" alt="Second slide">
                     <div class="carousel-caption">
                         <h3></h3>
                         <p></p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="..." alt="Second slide">
-                    <div class="carousel-caption">
-                        <h3></h3>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="..." alt="Third slide">
+                    <img src="http://lorempixel.com/640/400" alt="Third slide">
                     <div class="carousel-caption">
                         <h3></h3>
                         <p></p>
@@ -138,40 +75,27 @@ if (isset($_POST)) {
 
     <section class="container-fluid relative contact">
         <div class="container">
-            <h3>Say hi</h3>
-            <p>
-                Feel free to leave me a message, just to say hi or to get in touch.
-            </p>
-            <form class="contact-form" method="post">
-                <?php if (!empty($errors_php)) : ?>
-                    <div class="alert alert-danger" role="alert">
-                        <h3>Warning!</h3>
-                        <ul>
-                        <?php foreach ($errors as $errorname => $error) : ?>
-                            <li><?= $error ?></li>
-                        <?php endforeach; ?>
-                        </ul>
-                    </div>
-                <?php endif; ?>
-
+            <h2>Say hi</h2>
+            <p>Feel free to leave me a message, just to say hi or to get in touch.</p>
+            <form class="contact-form" action="validate.php" method="post">
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control form-control-danger" name="name" id="name" placeholder="What's your name?" autocomplete="off" value="<?= ($_POST && !empty($_POST['name'])) ? e($_POST['name']) : '' ; ?>">
+                    <input type="text" class="form-control form-control-success form-control-danger" name="name" id="name" placeholder="What's your name?" autocomplete="off" value="<?= ($_POST && !empty($_POST['name'])) ? e($_POST['name']) : '' ; ?>">
                 </div>
 
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="text" class="form-control form-control-danger" name="email" id="email" placeholder="What's your email?" autocomplete="off" value="<?= ($_POST && !empty($_POST['email'])) ? e($_POST['email']) : '' ; ?>">
+                    <input type="text" class="form-control form-control-success form-control-danger" name="email" id="email" placeholder="What's your email?" autocomplete="off" value="<?= ($_POST && !empty($_POST['email'])) ? e($_POST['email']) : '' ; ?>">
                 </div>
 
                 <div class="form-group">
                     <label for="message">Message</label>
-                    <textarea class="form-control form-control-danger" name="message" id="message" placeholder="Leave your message here"><?= ($_POST && !empty($_POST['message'])) ? e($_POST['message']) : '' ; ?></textarea>
+                    <textarea class="form-control form-control-success form-control-danger" name="message" id="message" placeholder="Leave your message here"><?= ($_POST && !empty($_POST['message'])) ? e($_POST['message']) : '' ; ?></textarea>
                 </div>
 
-                <div class="g-recaptcha" data-sitekey="6LeMQhgTAAAAAG1Dl2Is8XzbMjIkMv3SETGfPqRw"></div>
+                <!-- <div class="g-recaptcha" data-sitekey="6LeMQhgTAAAAAG1Dl2Is8XzbMjIkMv3SETGfPqRw"></div> -->
 
-                <input type="submit" class="btn btn-success" value="Send message">
+                <input type="submit" class="btn btn-outline-success btn-block" value="Send message">
 
             </form>
         </div>
@@ -179,14 +103,9 @@ if (isset($_POST)) {
 
     <footer>
         <div class="container">
-            <div class="col-sm-6 copyright">&copy; <?= date('Y') ?> Bas Hendriks </div>
-            <div class="col-sm-6"></div>
+            <div class="col-xs-8 copyright">&copy; <?= date('Y') ?> Bas Hendriks </div>
+            <div class="col-xs-4 social"><a href="https://www.linkedin.com/in/bbehendriks" target="_blank" class="block linkedin"></a></div>
         </div>
     </footer>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
-    <!-- Custom Javascript -->
-    <script src="js/min/main.js" charset="utf-8"></script>
-  </body>
-</html>
+<?php require_once 'includes/footer.php'; ?>
--- js/main.js
+++ js/main.js
@@ -1,49 +1,47 @@
 $(document).ready(function(){
     $('.quote').delay(300).fadeIn(1000);
 
-    var data = {};
+    // Form validation
+    $('form').submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
 
-    // http://michaelsoriano.com/how-to-ajax-validate-forms/
-    $('input[type="submit"]').click(function() {
         resetErrors();
 
-        var url = 'index.php';
+        // Serialize the form data.
+        var formData = $('form').serialize();
 
-        $.each($('form input, form textarea'), function(v, i) {
-            if (v.type !== 'submit') {
-                data[v.name] = v.value;
-            }
-        });
-
+        // Submit the form using AJAX.
         $.ajax({
             dataType: 'json',
-            type: 'post',
-            url: url,
-            data: data,
-            success: function(resp) {
-                if (resp === true) {
-                    $('form').submit();
-                    return false;
-                } else {
-                    $.each(rep, function(i, v) {
-                        console.log(i + ' => ' + v);
-                        var msg = '<div class="form-control-feedback">' + v + '</div>';
-                        $('input[name="' + i + '"], textarea[name="' + i + '"]').parent().addClass('has-danger');
-                        $('input[name="' + i + '"], textarea[name="' + i + '"]').after(msg);
-                    });
-                    var keys = Object.keys(resp);
-                    $('input[name="'+keys[0]+'"]').focus();
-                }
-                return false;
-            }, error: function() {
-                console.log('There was an error checking the fields');
+            type: 'POST',
+            url: $('form').attr('action'),
+            data: formData
+        })
+        .done(function(response) {
+            $('input[type="text"], textarea').parent().addClass('has-success');
+            $.each(response, function(i, v) {
+                console.log(i + ' => ' + v);
+                var msg = '<div class="form-control-feedback">' + v + '</div>';
+                $('input[name="' + i + '"], textarea[name="' + i + '"]').parent().removeClass('has-success');
+                $('input[name="' + i + '"], textarea[name="' + i + '"]').parent().addClass('has-danger');
+                $('input[name="' + i + '"], textarea[name="' + i + '"]').after(msg);
+            });
+
+            if (response.success) {
+                $('.form-group, input[type="submit"]').remove();
+                $('form').before('<div class="alert alert-success"></div>');
+                $('.alert').text(response.success)
             }
+        })
+        .fail(function(data) {
+            $('form').before('<div class="alert alert-danger"></div>').text(data.error);
+            $('.alert').text(data.error)
         });
-        return false;
     });
 });
 
 function resetErrors() {
-    $('form input, form textarea').removeClass('inputTxtError');
-    $('label.error').remove();
+    $('form input, form textarea').parent().removeClass('has-danger');
+    $('div.form-control-feedback, div.alert').remove();
 }
